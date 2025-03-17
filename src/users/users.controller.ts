213c94d5efc15b867ae05850c55ab0cdb4d6e0ba
CallMeChero <david.ceranic@gmail.com>
2025-03-17 20:00:28 +0100

    Users repo along with service & controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+
+    constructor(
+        private _usersService: UsersService
+    ) {}
+
+    @Post('/signup')
+    createUser(@Body() body: CreateUserDto) {
+        this._usersService.create(body.email, body.password)
+    }
+
+}
